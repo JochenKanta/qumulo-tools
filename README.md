4fe10278fae680b363dc36ce3d422aaa720efdeb
Tommy Unger <tommyunger@gmail.com>
2020-07-21 14:43:36 -0700

    adding capacity tree to csv script

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# qumulo-tools
-Fun Qumulo tools for the whole family
+# A smattering of Qumulo-oriented API tools
+
+## Write multiple levels of capacity tree data to a csv - `capacity-tree-data-to-csv.py`
+
+This script will take a start path and walk the tree below that path looking for directories. Each directory will be recorded with it's aggregate capacity below also recorded. Each level must have a name as well.
+
+*Example*: Let's say you have directory structure for states and counties:
+
+```
+Directory: /all-the-states
+    Directory: /all-the-states/WA
+        Directory: /all-the-states/WA/King
+        Directory: /all-the-states/WA/Pierce
+        Directory: /all-the-states/WA/Snohomish
+    Directory: /all-the-states/CO
+        Directory: /all-the-states/CO/Summit
+        Directory: /all-the-states/CO/Arapahoe
+```
+
+You would kick off the following command:
+`python capacity-tree-data-to-csv.py -s qumulo.storage -u admin -d /all-the-states -l state,county`
+
+* -s: The Qumulo host to run against
+* -u: The Qumulo api user
+* -d: The directory to start the walk from
+* -l: The name (or 'tag') for the level of directory. In this example: state = level 1, county = level 2
+* -p: Optional password on the command line, otherwise you'll be prompted.
+* -o: Optional path for the file output (default: qumulo-data-by-directory.csv in the working directory)
---
+++ capacity-tree-data-to-csv.py
@@ -0,0 +1,69 @@
+import argparse
+from getpass import getpass
+from datetime import datetime
+from qumulo.rest_client import RestClient
+
+
+def get_subdirs(rc, path):
+    subdirs = []
+    res = rc.fs.read_dir_aggregates(path = path, max_entries = 5000)
+    for ent in res["files"]:
+        if ent['type'] == 'FS_FILE_TYPE_DIRECTORY':
+            ent['path'] = path + ('/' if path != '/' else '') + ent['name']
+            subdirs.append(ent)
+    return sorted(subdirs, key = lambda d : d['name'])
+
+def get_line(ts, typ, ent):
+    line = "%(path)s,%(data_usage)s,%(num_files)s,%(num_directories)s" % ent
+    line = "%s,%s,%s\r\n" % (ts, typ, line)
+    return line
+
+
+def get_capacity_aggregates(rc, start_path, level_names, out_file_name):
+    timestamp = datetime.now().strftime("%Y-%m-%d")
+    level = 0
+    fw = open(out_file_name, "w")
+    fw.write("timestamp,directory_level,full_path,data_usage,file_count,directory_count\r\n")
+    dirs = [start_path]
+    while level < len(level_names):
+        next_dirs = []
+        while dirs:
+            current_dir = dirs.pop()
+            for ent in get_subdirs(rc, current_dir):
+                print("level+name+dir: %s %8s %s" % (level, level_names[level], ent['path']))
+                fw.write(get_line(timestamp, level_names[level], ent))
+                next_dirs.append(ent['path'])
+        level += 1
+        dirs = next_dirs
+    fw.close()
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Walk levels of a known Qumulo filesystem tree and gather capacity metrics'
+    )
+    parser.add_argument('-s', help='Qumulo hostname', required=True)
+    parser.add_argument('-u', help='Qumulo api user', required=True)
+    parser.add_argument('-p', help='Qumulo api passord')
+    parser.add_argument('-d', help='Starting directory path', required=True)
+    parser.add_argument('-o', help='Output file name', default='qumulo-data-by-directory.csv')
+    parser.add_argument('-l', nargs='+', help='Level names', required=True)
+    args, other_args = parser.parse_known_args()
+
+    if not args.p:
+        args.p = getpass()
+
+    creds = {"QHOST": args.s,
+             "QUSER": args.u,
+             "QPASS": args.p}
+    level_names = ['state', 'county']
+
+    # initialize the REST client
+    rc = RestClient(creds["QHOST"], 8000)
+    rc.login(creds["QUSER"], creds["QPASS"])
+
+    get_capacity_aggregates(rc, args.d, level_names, args.o)
+    print("Created file: %s" % args.o)
+
+if __name__ == "__main__":
+    main()
